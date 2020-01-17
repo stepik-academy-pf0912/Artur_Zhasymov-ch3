d5fc5f392c63734617c5f96e62ffdf82aac59f7f
i <ibelyalov@yandex.ru>
2020-01-17 05:00:48 +0300

    Implement

---
+++ code.py
@@ -0,0 +1,190 @@
+videos = [
+'Интервью с Backend JS Developer https://youtu.be/qQwcCS8K_K0',
+'Интервью с Python Developer https://youtu.be/VFBXx7O9BxU',
+'Интервью с Senior JavaScript Developer https://youtu.be/t0pYU50Bo6A',
+'Интервью с Frontend Team Lead / UX-Architect https://youtu.be/ZkJ2Q3zBvLU',
+'HTML для начинающих - #1 - Введение https://youtu.be/8mK5aY5YOCc',
+'HTML для начинающих - #2 - Теги и атрибуты https://youtu.be/oBKaXB9CMMo',
+'HTML для начинающих - #3 - Списки https://youtu.be/fgeANjiWwKg',
+'HTML для начинающих - #4 - Таблицы https://youtu.be/BYnpSIdqXt8',
+'HTML для начинающих - #5 - Фрэймы https://youtu.be/ydxgOZG0BUI',
+'HTML для начинающих - #6 - Ссылки https://youtu.be/nF0X9PRVOsA',
+'HTML для начинающих - #7 - Атрибуты.Часть 1 https://youtu.be/nxtV-PwYkjU',
+'HTML для начинающих - #8 - Атрибуты. Часть 2 https://youtu.be/jZR8m9djWXs',
+'HTML для начинающих - #9 - Формы. Введение https://youtu.be/abWbB4RYFYM',
+'HTML для начинающих - #10 - Формы. Часть 2 https://youtu.be/x55mwoVJ_IE',
+'HTML для начинающих - #11 - Семантические особенности HTML5 https://youtu.be/LOphfrzuxtc',
+'Основы CSS - #1 - Введение https://youtu.be/NkmZl1Yy94Q',
+'Основы CSS - #2 - Селекторы https://youtu.be/C8pO3Rbepgs',
+'Основы CSS - #3 - Box model, margin https://youtu.be/5ibNZAmyDV0',
+'Основы CSS - #4 - Структура сайта https://youtu.be/wQhN0SdSZuY',
+'Основы CSS - #5 - Подробно о каскадности https://youtu.be/7XaJDe5V2Mk',
+'Основы CSS - #6 - Использование шрифтов https://youtu.be/qssxQvRZ0mE',
+'Основы CSS - #7 - Цветовые модели и единицы измерения https://youtu.be/oRtpd-nci94',
+'Основы Javascript #1 - Типы данных https://youtu.be/n0sPFaLsNeI',
+'Основы Javascript #2 - Примитивные и объектные данные. Обертки https://youtu.be/p-Ob8gEJBJs',
+'Основы Javascript #3 - Statements (инструкции), expressions (выражения), operators (операторы) https://youtu.be/hqozKP8pol8',
+'Основы Javascript #4 - Условные и логические операторы https://youtu.be/ogmqkRFNmcE',
+'Основы Javascript #5 - Числа https://youtu.be/IKddd9ZTwB4',
+'Основы Javascript #6 - Строки https://youtu.be/BTzuzSLsai8',
+'Основы Javascript #7 - Boolean, null, undefined https://youtu.be/vyC1BTyh-E0',
+'Основы Javascript #8 - Преобразования типов данных https://youtu.be/OhldHbz93Kw',
+'Основы Javascript #9 - Объекты https://youtu.be/sfqsBd6Iwx0',
+'Основы Javascript #10 - Массивы https://youtu.be/5hjC9Y0WAig',
+'Основы Javascript #11 - Циклы https://youtu.be/kFDhIxBV2cs',
+'Основы Javascript #12 - Функции https://youtu.be/FZkSwa_rm_E',
+'Основы Javascript #13 - Область видимости переменных (scope) https://youtu.be/RlTcSgNph68',
+'Основы Javascript #14 - Замыкание (closure) https://youtu.be/N09Ljaaexqk',
+'Основы Javascript #15 - Объект window и BOM (объектная модель браузера) https://youtu.be/mzoe-I7r5HE',
+'Основы Javascript #16 - DOM (объектная модель документа) https://youtu.be/1FpuKVmotNc',
+'Основы Javascript #17 - Создание элементов динамически https://youtu.be/v2nLPYTmxBo',
+'Основы Javascript #18 - Изменение стилей элемента https://youtu.be/NDe-anrOZ_s',
+'Основы Javascript #19 - Анимация и таймеры https://youtu.be/-CGo9A7_oEg',
+'Основы Javascript #20 - Знакомство с событиями https://youtu.be/Ojcp828IAfw',
+'Основы Javascript #21 - Стандартная модель событий https://youtu.be/0udLlA5Y7yc',
+'Основы Javascript #22 - Модель событий Internet Explorer https://youtu.be/OGs4L59vVS4',
+'Основы Javascript #23 - Кросс-браузерные события https://youtu.be/FFlRoNYXEW0',
+'Основы Javascript #24 - Делегирование событий https://youtu.be/07pqPoYbY0k',
+'1. Введение, компонентный подход https://youtu.be/ol4OVMJZC1w',
+'2. Настройка окружения, create-react-app https://youtu.be/2op_HAh1j3I',
+'3. JSX - синтаксис для разметки в React.js https://youtu.be/CqkiEayFzpY',
+'4. React-компонент, передача данных https://youtu.be/Os_cEPPewxA',
+'5. Интерактивность, состояние компонентов https://youtu.be/UNEp4uXY9TA',
+'6. Virtual DOM - основной алгоритм React.js https://youtu.be/gb-93KcTRFo',
+'7. Отображение массивов, смысл аттрибута key https://youtu.be/tn9HyYRVZ9A',
+'8. Оформление компонентов, работа с CSS https://youtu.be/xAO--NOX3PY',
+'9. Жизненный цикл React-компонентов https://youtu.be/i2NE0cbuQa4',
+'10. Оптимизация приложений, shouldComponentUpdate https://youtu.be/Jw1zocLDnnc',
+'11. Отладка React-приложений https://youtu.be/NhT5nMvve4Q',
+'12. Обратный поток данных, подъём состояния https://youtu.be/qdaDgC_xU0w',
+'React и Redux 1. Что это за Redux и зачем он нужен https://youtu.be/jvkhKRHS2TY',
+'React и Redux 2. Подключаемся к Redux https://youtu.be/wzWZDh0dUYE',
+'React и Redux 3. Структура приложения, combineReducers, контейнеры https://youtu.be/MahSyZSFQv8',
+'Node.js. Урок 1. Введение в платформу Node.js https://youtu.be/fch6fdXHIxA',
+'Node.js. Урок 2. Обработка событий https://youtu.be/owvEts9H440',
+'Node.js. Урок 3. Работа в консоли https://youtu.be/aRgkRBjy0h8',
+'Node.js. Урок 4. Работа с http запросами https://youtu.be/IXMK81tGUbw',
+'Node.js. Урок 5. Фреймворк Express https://youtu.be/Zyb0G8ADmSs',
+'Node.js. Урок 6. Работа с базами данных https://youtu.be/P87LNAfPtIc',
+'Node.js. Урок 7. Работа с сессиями и cookies https://youtu.be/4amKSSJUlxs',
+'Node.js. Урок 8. Аутентификация и авторизация https://youtu.be/qMlFmcR5rDI',
+'Node.js. Урок 9. REST API https://youtu.be/A_8ns2zTVLM',
+'Node.js. Урок 10. Socket.IO https://youtu.be/Rfh1pnVOpRQ',
+'Урок 1 Установка Python 3.7 на Windows https://youtu.be/IU4-19ofajg',
+'Урок 2 Варианты выполнения кода. Первая программа на Python https://youtu.be/mOQBZq9WCCY',
+'Урок 3 Знакомство с объектами. Числа и операции над ними Python https://youtu.be/fF1ZqTKMR6I',
+'Урок 4 Переменные в Python. Оператор присваивания https://youtu.be/KP4mhrDxaWw',
+'Урок 5 Ввод и вывод данных python. Команда input() https://youtu.be/yuyq2BrHMJE',
+'Урок 6 Ввод и вывод данных Python. Функция print() Python https://youtu.be/xVEuHyR2ePA',
+'Урок 7 Деление нацело и деление по остатку Python https://youtu.be/RlfSygvBeZE',
+'Урок 8 Логический тип Bool. Операции сравнения Python https://youtu.be/vLgf3wJzmAg',
+'Урок 9 Cтроки и операции над ними Python https://youtu.be/YUMJxjsfNt4',
+'Урок 10 Строки: индексы и срезы Python https://youtu.be/ITI2kJ6j8Ho',
+'Урок 11 Строки и их методы Python https://youtu.be/GmMD6gQYWe4',
+'Урок 12 Списки и операции над ними Python https://youtu.be/CEQZYZMPJSU',
+'Урок 13 Списки: индексы и срезы Python https://youtu.be/z8WWOnxUOb8',
+'Урок 14 Списки и их методы Python https://youtu.be/e0If6CJZs4s',
+'Урок 15 Условный оператор if Python https://youtu.be/EggJRTzid1M',
+'Урок 16 Вложенный оператор if Python https://youtu.be/8YshxYHIeeI',
+'Урок 17 Множественный выбор elif Python https://youtu.be/8F-EfhsKHCI',
+'Урок 18 Цикл while Python https://youtu.be/Ll3AN1FXXfE',
+'Урок 19 Обход всех цифр числа с помощью while Python https://youtu.be/wZMhYfwpel0',
+'Урок 20 Цикл while Алгоритм Евклида Python https://youtu.be/yrOKPlg1TXg',
+'Урок 21 Цикл while. Нахождение всех делителей числа Python https://youtu.be/ygA8EMHbtxU',
+'Урок 22 Цикл while. Инструкции break, continue, else https://youtu.be/Myh7OdxoYsA',
+'Урок 23 Функция range и итерируемые объекты https://youtu.be/9J0fvF4k4F4',
+'Урок 24 Цикл for. Обход элементов функции range Python https://youtu.be/yPUA8xBEyzM',
+'Урок 25 Цикл for. Обход списков и строк Python https://youtu.be/iopPsTT7Pes',
+'Урок 26 Установка, настройка и использование PyCharm https://youtu.be/uWLwvhbbEe4',
+'Урок 27 Метод подсчета. Сортировка подсчетом Python https://youtu.be/ukTBUyTOZs4',
+'Урок 28 Вложенные циклы Python https://youtu.be/tsVgSwSdsa8',
+'Урок 29 Вложенные списки Python https://youtu.be/0qtLrRm36J0',
+'Урок 30 Треугольник Паскаля Python. Коэффициенты для Бинома Ньютона https://youtu.be/YfW-s3V4-c8',
+'Урок 31 Множества в Python. Тип данных set https://youtu.be/KMGRXDxUw18',
+'Урок 32 Словари (dict) Python. Операции и методы словаря https://youtu.be/7_Zrh1--d5o',
+'Урок 33 Ситуации, где полезно использовать словарь Python https://youtu.be/dZ474oWml9k',
+'Урок 34 Кортежи (tuple) в Python. Операции и методы кортежей https://youtu.be/pf3V5rr30Ts',
+'Урок 35 Функции (def) в Python. Определение и вызов функции https://youtu.be/DJAlfolEv9A',
+'Урок 36 Зачем нужны функции в программировании Python https://youtu.be/pL01GSH_XJk',
+'Урок 37 Возвращаемое значение функции. Оператор return Python https://youtu.be/cWyUODRGIDY',
+'Урок 38 Область видимости. Локальная, глобальная и встроенная области видимости Python https://youtu.be/8NDjlmCRCsk',
+'Урок 39 Передача аргументов Python. Сопоставление аргументов по имени и позиции https://youtu.be/kFHIEjLyh8g',
+'Урок 40 *args и **kwargs Python. Передача аргументов в функцию https://youtu.be/mcAB5dBXMp4',
+'Урок 41 Рекурсия в Python. Рекурсивная функция Часть 1 https://youtu.be/jvFULnNpNLg',
+'Урок 42 Рекурсия в Python. Рекурсивная функция Часть 2 https://youtu.be/rzGCxtZdMuM',
+'Урок 43 Рекурсивный обход файлов Python https://youtu.be/wZvk8nyPQCY',
+'Урок 44 Функция enumerate Python https://youtu.be/Hz1lDs69cCs',
+'Урок 45 Lambda функция Python. Lambda выражение. Анонимная функция Lambda https://youtu.be/8fzrm1tX5lI',
+'Урок 46 Генераторы списков Python | List comprehension https://youtu.be/_zBTBr6XdZo',
+'Урок 47 Генераторы списков Python 2 | Вложенные генераторы списков https://youtu.be/_RA35zG-0gA',
+'Урок 48 Генераторы и итераторы. Выражения -генераторы в Python https://youtu.be/vn6bV6BYm7w',
+'Урок 49 Функция генератор. Создание генератора при помощи yield Python https://youtu.be/8cMMO8fks-k',
+'Урок 50 Функция map в Python https://youtu.be/2ghKShXWuSs',
+'Урок 51 Функция filter в Python https://youtu.be/q0xKow-E4Ws',
+'Урок 52 Функция zip python. Что делает функция zip в Python https://youtu.be/1sNJoGQQfiM',
+'Урок 53 Сортировка коллекций в Python. Метод sort и функция sorted https://youtu.be/Qi5HEyla4_U',
+'Урок 54 Сортировка по ключу Python. Аргумент key | Сортировка Python https://youtu.be/I4u5dth91G0',
+'Метод split и метод join Python https://youtu.be/M5DB3Yun8gU',
+'Экранированные символы Python. Служебные символы в Python https://youtu.be/W8p7S_WVyec',
+'Урок 55 Установка модулей в Python || Установка пакета Python https://youtu.be/BXaBQ0PcNTk',
+'Урок 56 Функции all и any. Программирование Python https://youtu.be/TdFTY_xgHvw',
+'Форматирование строк Python. Метод format строк https://youtu.be/Yab7gE8K1Ko',
+'F-строка. F-string. Программирование на Python https://youtu.be/QlkQNWQUCLc',
+'1. Cоздание блога на Flask (уроки) - "Демка проекта" https://youtu.be/Y_oyx36AdV0',
+'2. Создание блога на Flask (уроки) - Подготовка к работе https://youtu.be/-52bQowjOAA',
+'3. Создание блога на Flask (уроки) - Cоздание приложения, HTML шаблоны https://youtu.be/y1edhgne48g',
+'4. Создание блога на Flask (уроки) - Наследование шаблонов https://youtu.be/oixGHKxv4y0',
+'5. Создание блога на Flask (уроки) - Blueprints https://youtu.be/Hkiy_eSvzIk',
+'6. Cоздание блога на Flask (уроки) - Cоздание постов (модели) и SQLAlchemy https://youtu.be/Y8i_UjuqunQ',
+'7. Создание блога на Flask (уроки) - Миграции, теги, связь тегов и постов https://youtu.be/poz824J33yg',
+'8. Создание блога на Flask (уроки) - Поиск и форма создания поста https://youtu.be/h08XNlQwh4Q',
+'9. Создание блога на Flask (уроки) - Добавление постраничной навигации https://youtu.be/Ler7PRDknTs',
+'10. Создание блога на Flask (уроки) - Форма редактирования постов и админка https://youtu.be/VqjfPescRhA',
+'11. Создание блога на Flask (уроки) - Авторизация админа, базовая кастомизация админки https://youtu.be/wDBiMtKIBs0',
+'12. Создание блога на Flask (уроки) - Страница 404, кастомный CSS https://youtu.be/xN7fnCuCItk'
+]
+
+playlists = [
+'HTML для начинающих | loftblog',
+'Основы CSS | loftblog',
+'Основы Javascript | loftblog',
+'Основы React.JS | JavaScript.ru',
+'React и Redux | knowcity',
+'Node.js | ITVDN',
+'Интервью',
+'Язык программирования Python | egoroff_channel',
+'Cоздание блога на Flask | Олег Молчанов'
+]
+
+hello_message = 'Привет, это Stepik Code Academy, портал обучающих видео про веб-разработку.'
+ask_message = 'Введите 1, чтобы посмотреть видео, 2 чтобы найти видео, 3 чтобы посмотреть плейлисты, exit или 0, чтобы выйти.'
+
+videos_report_message = 'У нас нашлось {} видео:'
+search_ask_message = 'Введите слово для поиска: '
+search_report_message = 'У нас нашлось {} видео по запросу "{}"'
+playlists_report_message = 'У нас нашлось {} плейлист(-ов):'
+
+print(hello_message)
+
+while True:
+    user_request = input(ask_message + '\n')
+
+    if user_request == '1':
+        print(videos_report_message.format(len(videos)))
+        for video in videos:
+            print(video)
+    elif user_request == '2':
+        search_request = input(search_ask_message)
+        search_result = []
+        for video in videos:
+            if search_request in video:
+                search_result += [video]
+        print(search_report_message.format(len(search_result), search_request))
+        for video in search_result:
+            print(video)
+    elif user_request == '3':
+        print(playlists_report_message.format(len(playlists)))
+        for playlist in playlists:
+            print(playlist)
+    elif user_request == 'exit' or '0':
+        break
